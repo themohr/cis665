7e857ec3c32b5a21e552c9d085462126ad67e3a1
Hemang.Vyas12 <Hemang.Vyas12@BusElab11.BUSINESS.ColoState.EDU>
2016-03-01 11:32:42 -0700

    Initial DAO and Model layer classes

---
+++ project/php/commons/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * Authors: Dennis Mohr, Nick Hoyle, Hemang Vyas
+ * Date: 02/28/2016
+ * Description: Constants
+ *  
+ */
+
+
+
+define("ADD","ADD");
+define("UPDATE","UPDATE");
+define("DELETE","DELETE");
+define("SUCCESS","SUCCESS");
+define("ERROR","ERROR");
+
+?>
